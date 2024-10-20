58e9ebd632c9c43db5fbc5b4086cf57f1dc4ce73
dOtOb9 <masa.taga0528@gmail.com>
2024-10-20 22:59:12 +0900

    横軸の時間の目盛りを調整

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from matplotlib.dates import MonthLocator, DateFormatter
+
 sns.set_style('darkgrid')
 
 
 df = pd.read_csv('atcoder_score_data.csv')
+
+df['date'] = pd.to_datetime(df['date'])
 
 fig, ax = plt.subplots(1, 1)
 
@@ -24,8 +28,15 @@ ax.axhspan(ymin=2800, ymax=4000, color='red', alpha=0.2) # 赤色
 
 ax.set_ylim(bottom=0, top=max(max(df['performance']), max(df['rate']))*1.2)
 
+ax.xaxis.set_major_locator(MonthLocator())
+ax.xaxis.set_major_formatter(DateFormatter('%Y-%m'))
+
+plt.xticks(rotation=90)
+
 ax.set_title('AtCoder Score History')
 ax.legend(loc='upper left')
+
+plt.tight_layout()
 
 plt.savefig('./output/fig.png')
 plt.show()
